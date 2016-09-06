69bd2b4de1c45213e977908e5c2fe1b9997d62e8
gsubiran <gsubiran@hotmail.com>
2016-09-06 09:54:28 -0300

    Se agregaron un poco mas de comentarios al script
    Se agregaron comentarios y se soluciono un detalle con respecto al conteo de intentos fallidos a la hora de ingresar un valor incorrecto para  ingresar el dia desde/dia hasta. El mensaje estaba mostrando la cantidad de intentos fallidos siempre en 0. Arreglado

--- fastHoursCharge.js
+++ fastHoursCharge.js
@@ -2,9 +2,13 @@
 (function CargaRapidaDeHoras(){
 alert("ATENCION!!! Esta es solo una herramienta en fase de prueba que ayuda a cargar los campos de las horas rapidamente en el sitio del ERP. \r\n Esto no suplanta en ningun momento los procedimientos existentes. Usela bajo su propio riesgo y no olvide validar que los datos sean los que esperaba antes presionar el boton guardar.");
 
+//Selector de los divs que contienen informacion del proyecto
 var proyectosNombreSelector = ".oe_timesheet_weekly_account:has(a)";
 var domArrayProyectos = $(proyectosNombreSelector);
 
+//Array para almacenar las tareas
+//cada objeto tarea contendra el nombre del proyecto, el nombre de la propia tarea y luego los
+//objetos del dom correspondientes a los inputs de cada día del mes.
 var myArrayTareas = [];
 
 //Se itera en el dom para obtener informacion sobre las tareas existentes
@@ -19,11 +23,10 @@ for (var idx = 0; idx < domArrayProyectos.length; idx++) {
 		Proyecto: proyNombre,
 		Tarea: tareaNombre
 	});
-	console.log("Proyecto '" + proyNombre + "' - " + "tarea '" + tareaNombre + "'.");
-	//var  $($($(proyectosNombreSelector)[0]).next().find("a")[0]);
+	//console.log("Proyecto '" + proyNombre + "' - " + "tarea '" + tareaNombre + "'.");
 }
 
-console.log("Tareas: ",myArrayTareas);
+//console.log("Tareas: ",myArrayTareas);
 
 var domAllFechas = domArrayProyectos.closest("table").parent().next().children("table");
 var cantidadDiasMes = 0;
@@ -41,18 +44,20 @@ for (var idx = 0; idx < myArrayTareas.length; idx++) {
 		cantidadDiasMes=arrayFechasItemTarea.length;
 	}
 }
-console.log("Cantidad dias mes: ",cantidadDiasMes);
-console.log("Tareas: ",myArrayTareas);
+//console.log("Cantidad dias mes: ",cantidadDiasMes);
+//console.log("Tareas: ",myArrayTareas);
 
 var numeroDiaDesde = null;
-var intentosPosibles = 5;
+var IntentosPosibles = 5;
+var intentosRestantes = IntentosPosibles;
 
+//Recoleccion de datos para el dia desde
 while(isNaN(parseInt(numeroDiaDesde))
 	|| (parseInt(numeroDiaDesde) <=0
 	|| parseInt(numeroDiaDesde) > cantidadDiasMes)){
 	
-	if(intentosPosibles===0){
-		alert("Ingresaste un valor incorrecto " + intentosPosibles + " veces. Tienes problemas o estas jugando? se seteara el valor por defecto 1.");
+	if(intentosRestantes===0){
+		alert("Ingresaste un valor incorrecto " + IntentosPosibles + " veces. Tienes problemas o estas jugando? se seteara el valor por defecto 1.");
 		numeroDiaDesde = 1;
 		break;
 	}
@@ -62,21 +67,24 @@ while(isNaN(parseInt(numeroDiaDesde))
 	}
 	numeroDiaDesde = prompt("Cargar desde el dia: ", "1");
 	
-	intentosPosibles--;
+	intentosRestantes--;
 }
 numeroDiaDesde=parseInt(numeroDiaDesde);
 
-intentosPosibles=5;
+//Se reasigna la variable para volver el contardor de intentos restantes a cero 
+//para obtener los datos del siguiente parametro
+intentosRestantes=IntentosPosibles;
 
 var numeroDiaHasta = null;
 
+//Recoleccion de datos para el dia hasta
 while(numeroDiaHasta === null
 || (numeroDiaHasta.toLowerCase() !== "hoy" && isNaN(parseInt(numeroDiaHasta)))
 || ( parseInt(numeroDiaHasta) <= 0
 || parseInt(numeroDiaHasta) > cantidadDiasMes)){
 	
-	if(intentosPosibles===0){
-		alert("Ingresaste un valor incorrecto " + intentosPosibles + " veces. Tienes problemas o estas jugando? se seteara el valor por defecto 'hoy'.");
+	if(intentosRestantes===0){
+		alert("Ingresaste un valor incorrecto " + IntentosPosibles + " veces. Tienes problemas o estas jugando? se seteara el valor por defecto 'hoy'.");
 		numeroDiaHasta = "hoy";
 		break;
 	}
@@ -104,8 +112,11 @@ for (var idxDia = (numeroDiaDesde -1); idxDia < cantidadDiasMes ; idxDia++) {
 		
 		//Validar si es el dia de hoy
 		if(domItemDiaTarea.hasClass("oe_timesheet_weekly_today")){
+			//En este punto podemos decir
+			//que se trata del día de hoy o de un finde
 			var dateToday = new Date();
 			if(dateToday.getDate() === (idxDia+1)){
+				//Significa que es el dia de hoy
 				banderaDateToday = true;
 			}
 			
@@ -116,29 +127,42 @@ for (var idxDia = (numeroDiaDesde -1); idxDia < cantidadDiasMes ; idxDia++) {
 			}
 		}
 		
-		
+		//Seleccion del campo input para el dia y la tarea actual
 		var domItemDiaTareaInput = domItemDiaTarea.children("input");
 		
+		//Valor de las horas existentes en el campo
 		var horasExistentesDiaTarea = domItemDiaTareaInput.val();
+		
+		//Informacion a desplegar
 		var mensajeAMostrar =  "(info: escriba 'salir' en cualquier momento para terminar.)\r\n"
 							   + "Linea = " + (idxTarea+1) + " \r\n "
 							   + "Día Nº " + (idxDia+1) + " - horas actuales: "+ tareasAnterioresHs + " \r\n "
 							   + "Proyecto: " + myItemTarea.Proyecto + " \r\n "
 							   + "Tarea: " + myItemTarea.Tarea + " \r\n "
 							   + "Especifique las horas: ";
+		//Datos ingresados por el usuario					   
 		var horasDiaTareaIngresada = prompt(mensajeAMostrar,(horasExistentesDiaTarea==0 ? "00:00" : horasExistentesDiaTarea));
 		
+		//Keyword para poder salir del loop en cualquier momento
 		if (horasDiaTareaIngresada.toLowerCase() === "salir"){
 			banderaUserFuerzaSalir= true;
 			break;
 		}
 		
+		//Aqui se iran manteniendo los valores de las horas que se van ingresando, para el dia actual
+		//de este modo el usuario puede ver que valores tiene ingresados para el dia actual en otras tareas
 		tareasAnterioresHs = tareasAnterioresHs + (tareasAnterioresHs==="" ? "" : " + " )  + horasDiaTareaIngresada;
-				
+		
+		//Asignacion de horas para la tarea y dia actuales		
 		domItemDiaTareaInput.val(horasDiaTareaIngresada);
 		
+		//Llamado al evento change() del input 
+		//IMPORTANTE: esto es lo que nos bloquea la UI cuando cargamos horas normalmente, solo que
+		//aqui no nos va a hacer problema dado que los prompt y alerts pausan la ejecucion del javascript
+		//haciendo que no se propague rapidamente este evento, una vez terminado el "wizard" estos eventos tendran lugar
+		//haciendo de una vez el loading del ERP y no cada vez que ingresamos un valor.
 		domItemDiaTareaInput.change();//Se llama al change() para que el ERP tome que el campo fue cambiado
-		//$('<div id="dialog" title="Basic dialog"><p>Lala</p></div>').dialog();
+		
 	}
 	
 	//Usuario forzo la salida
@@ -147,15 +171,17 @@ for (var idxDia = (numeroDiaDesde -1); idxDia < cantidadDiasMes ; idxDia++) {
 		break;
 	}
 	
+	//Menejo de la keyword hoy
 	if(numeroDiaHasta.toLowerCase() === "hoy"
 		&& banderaDateToday){
 			alert("Carga hasta el dia de hoy finalizada, no olvide guardar los datos!");
 			//Carga hasta hoy
 			break;
 	}	
-	console.log("numeroDiaHasta: ", numeroDiaHasta);
-	console.log("Dia N: ", (idxDia+1));
+	//console.log("numeroDiaHasta: ", numeroDiaHasta);
+	//console.log("Dia N: ", (idxDia+1));
 	
+	//Mensaje para recordar al usuario hasta que Nº de dia hizo la carga
 	if(numeroDiaHasta == (idxDia+1)){
 		alert("Carga hasta el dia " + numeroDiaHasta + " finalizada, no olvide guardar los datos!")
 		//Carga hasta el dia establecido
